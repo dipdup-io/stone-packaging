1b0c97810c0c827405d00b826f0fde33e6e2eb0e
dm <dm@baking-bad.org>
2024-08-16 20:33:50 +0300

    Add the release job

---
+++ build_mac.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+brew install gmp
+
+python3 -m pip install cpplint pytest numpy sympy==1.12.1 cairo-lang==0.12.0
+
+git clone https://github.com/baking-bad/stone-prover.git /tmp/stone-prover
+
+cd /tmp/stone-prover || exit
+
+bazelisk build //...
+
+bazelisk test //...
