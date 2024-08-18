051c40610cd79695950277d0cdedeb78fd3f1ade
dm <dm@baking-bad.org>
2024-08-19 00:30:04 +0300

    Skip cairo-run to avoid MacOS x64 failure

--- build_and_test.sh
+++ build_and_test.sh
@@ -40,20 +40,7 @@ bazelisk test //...
 ln -s /tmp/stone-prover/build/bazelbin/src/starkware/main/cpu/cpu_air_prover /usr/local/bin/cpu_air_prover
 ln -s /tmp/stone-prover/build/bazelbin/src/starkware/main/cpu/cpu_air_verifier /usr/local/bin/cpu_air_verifier
 
-cd /tmp/stone-prover/e2e_test/CairoZero || exit
-
-cairo-compile fibonacci.cairo --output fibonacci_compiled.json --proof_mode
-
-cairo-run \
-    --program=fibonacci_compiled.json \
-    --layout=small \
-    --program_input=fibonacci_input.json \
-    --air_public_input=fibonacci_public_input.json \
-    --air_private_input=fibonacci_private_input.json \
-    --trace_file=fibonacci_trace.json \
-    --memory_file=fibonacci_memory.json \
-    --print_output \
-    --proof_mode
+cd /tmp/stone-prover/test_files || exit
 
 cpu_air_prover \
     --out_file=fibonacci_proof.json \
