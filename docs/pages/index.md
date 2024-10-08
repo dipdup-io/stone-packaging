c591e4bd89159edbdf93a1626b1b6c1e360300ce
Dmitry Mirgaleev <dm@baking-bad.org>
2024-10-08 20:23:07 +0300

    Clean up, add sidebar

--- docs/pages/index.md
+++ docs/pages/index.md
@@ -4,6 +4,28 @@ title: Introduction
 
 # Stone Packaging
 
-This is the main page of Stone Packaging documentation.
+This project provides various forms of distribution for [Stone](https://github.com/starkware-libs/stone-prover) executables.
 
-Here's an example of how to use Stone Packaging...
+## About
+
+The goal of this project is to reduce friction and speed up the process of generating proofs using Stone. More broadly, the aim is to make Stone a &#34;known&#34; piece of infrastructure that can be easily integrated into application-specific workflows and maintained efficiently.
+
+## Roadmap
+
+- [x] Static binary releases for x86_64
+- [x] Static binary releases for ARM64
+- [x] Minimal Docker images for x86_64
+- [x] Native packages for Debian/Ubuntu
+- [x] Native packages for Fedora
+- [x] Homebrew packages
+
+Follow-up work:
+
+- Native packages for Alpine
+- Technical documentation for file formats (inputs, outputs, memory, trace, proof), and test data
+- Documentation hosted on GitHub Pages
+- Integrated proof decomposition (related to https://github.com/zksecurity/stark-evm-adapter)
+- Observability suite (metrics, dashboards, configurable logging)
+- Stwo support
+
+This project is supported by Nethermind and Starknet Foundation via [OnlyDust platform](https://app.onlydust.com/p/stone-packaging-)
--- vocs.config.ts
+++ vocs.config.ts
@@ -3,19 +3,39 @@ import { defineConfig } from 'vocs'
 export default defineConfig({
   title: 'Stone Packaging',
   rootDir: 'docs',
-  
+
   sidebar: [
     {
-      text: 'Introduction',
-      link: '/',
+      text: "Overview",
+      link: "/",
     },
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      text: 'Installation',
+      collapsed: true,
+      items: [
+        { text: "From sources", link: "/install/sources" },
+        { text: "Static binaries", link: "/install/binaries" },
+        { text: "Docker images", link: "/install/docker" },
+        { text: "Debian packages", link: "/install/debian" },
+      ],
     },
     {
-      text: 'Example',
-      link: '/example',
+      text: 'Usage',
+      collapsed: true,
+      items: [
+        { text: "Generating execution artifacts", link: "/usage/execution" },
+        { text: "Stone Prover configuration", link: "/usage/configuration" },
+        { text: "Proving execution", link: "/usage/proving" },
+        { text: "Verifying proof", link: "/usage/verifying" },
+      ],
+    },
+    {
+      text: 'Advanced',
+      collapsed: true,
+      items: [
+        { text: "Verifying Stone proof in Ethereum", link: "/advanced/ethereum" },
+        { text: "Verifying Stone proof on Starknet", link: "/advanced/starknet" },
+      ],
     },
     {
       text: 'Stone Packaging Resources',
