82beb678c8e5e47b94781f2aaad00cd553c7f0d8
Zaariel91 <115039613+Zaariel91@users.noreply.github.com>
2024-10-21 21:25:55 +0300

    Update alpine-build.sh

--- alpine-build.sh
+++ alpine-build.sh
@@ -5,20 +5,20 @@ set -e
 os=$(uname | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 
-apk update
-apk add --no-cache ncurses elfutils-dev gmp-dev build-base python3 python3-dev py3-pip libffi-dev cairo-dev
+# apk update
+# apk add --no-cache ncurses elfutils-dev gmp-dev build-base python3 python3-dev py3-pip libffi-dev cairo-dev
 
-# python3 -m venv venv
-# chmod 777 -R venv/
-# ./venv/bin/activate
+# # python3 -m venv venv
+# # chmod 777 -R venv/
+# # ./venv/bin/activate
 
-# pip3 --version
-# pip3 install --upgrade pip
+# # pip3 --version
+# # pip3 install --upgrade pip
 
 
-#pip install --user pipx
-#export PATH="$HOME/.local/bin:$PATH"
-pip3 install cpplint pytest numpy sympy==1.12.1 cairo-lang==0.12.0
+# #pip install --user pipx
+# #export PATH="$HOME/.local/bin:$PATH"
+# pip3 install cpplint pytest numpy sympy==1.12.1 cairo-lang==0.12.0
 
 wget "https://github.com/bazelbuild/bazelisk/releases/download/v1.20.0/bazelisk-$os-$arch"
 chmod 755 "bazelisk-$os-$arch"
