2350888bd70d792d90f0e95a47dff6e04a47f0aa
Zaariel91 <115039613+Zaariel91@users.noreply.github.com>
2024-10-21 18:53:55 +0300

    Update alpine-build.sh

--- alpine-build.sh
+++ alpine-build.sh
@@ -5,7 +5,7 @@ set -e
 os=$(uname | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 
-apk add --no-cache ncurses elfutils-dev gmp-dev python3 python3-dev py3-pip build-base cairo-dev
+apk add --no-cache ncurses elfutils-dev gmp-dev build-base
 
 # python3 -m venv venv
 # chmod 777 -R venv/
